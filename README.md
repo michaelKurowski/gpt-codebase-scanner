d9edc24a0da68a375a3e80abed0cb6229c5f0add
michaelKurowski <Byamarro@gmail.com>
2023-03-28 17:10:40 +0300

    somewhat working example

--- README.md
+++ README.md
@@ -4,3 +4,23 @@ This is an utility that scans a typescript codebase and allows user to query it 
 # Requirments
 - OpenAPI API Key
 - Node.js
+
+# Examples
+```
+> npx ts-node --esm ./index.ts ./data/repo/
+
+Vector store exists, loading...
+
+
+Please ask anything, type "exit" to close:
+
+User: What does this program even do?
+
+AI: Based on the provided code snippets, it seems like a program that builds a simple console-based Snake game in TypeScript. The program defines the game's logic and renderings and includes dependencies on utility functions. It initializes a Board as a 2D Array of 10x10, sets the starting position of the snake, and generates a random location for a food chunk. The snake moves as directed by the user input, and the food chunk generates at a new location as the snake consumes it. When the snake goes beyond the game board, the game ends.
+
+Please ask anything, type "exit" to close:
+
+User: How does the author implement arithmetics?
+> 
+AI: From the provided code snippets, it seems like the author implements arithmetics in the `./utils/arithmetics.ts` module. This module exports functions like `incrementSingleDigit` and `decrementSingleDigit`, as well as a type `DoubleDigit`. The `decrement` function takes a `DoubleDigit` and decrements it using the `decrementSingleDigit` function. Additionally, the `parseSingleDigit` and `parseDigits` types in the `./utils/typesConversions.ts` module can be used to parse numerical strings into arrays of digits, potentially used in arithmetic operations.
+```
---
+++ src/index.ts
@@ -0,0 +1,154 @@
+import * as dotenv from 'dotenv' 
+import fs from 'node:fs'
+import { TextLoader } from "langchain/document_loaders";
+import { ChatOpenAI } from "langchain/chat_models";
+import { OpenAIEmbeddings } from "langchain/embeddings";
+import { HNSWLib } from "langchain/vectorstores";
+import dedent from 'dedent'
+import { HumanChatMessage, SystemChatMessage } from 'langchain/schema';
+import ts from 'typescript'
+import meow from 'meow';
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+import path from 'node:path';
+
+dotenv.config()
+
+if (!process.env.OPEN_AI_KEY?.length) {
+  throw new Error("OpenAI API Key is not set");
+}
+
+const USAGE = dedent`
+Usage:
+  $ code-query <path to repository>
+
+Options:
+  --clear-cache, -c         Clears cache
+`
+
+const cli = meow(
+  USAGE,
+  {
+	importMeta: import.meta,
+	flags: {
+		clearCache: {
+			type: 'boolean',
+			alias: 'c'
+		}
+	}
+})
+const codebaseDirectory = cli.input[0]
+if (!codebaseDirectory) {
+  console.error(dedent`
+    No file path provided. Please provide a path to a codebase.
+    ${USAGE}
+  `)
+  process.exit(1)
+}
+
+if (cli.flags.clearCache) {
+  console.log('Clearing cache...')
+  try {
+    fs.rmSync('./vector-store', { recursive: true })
+  } catch(err) {
+    throw new Error(`Failed to clear cache: ${err}`)
+  }
+}
+
+const vectorStore = await loadVectorStore(codebaseDirectory)
+
+const chat = new ChatOpenAI({
+  openAIApiKey: process.env.OPEN_AI_KEY,
+  modelName: "gpt-3.5-turbo"
+});
+
+const rl = readline.createInterface({ input, output });
+
+
+
+
+
+while(true) {
+  const answer = await rl.question('\n\nPlease ask anything, type "exit" to close:\n\nUser: ');
+  if (answer === 'exit') {
+    console.log('\n\nBye')
+    rl.close();
+    process.exit(0)
+  }
+  const resultOne = await vectorStore.similaritySearch(answer, 3);
+  const response = await chat.call([
+    new SystemChatMessage(`You're an AI. You answer questions about codebase and draw mermaid diagrams. The codebase has been already scanned and relevant code snippets have been prepared for you. This is the relevant data for the user's question: \n${resultOne.map(x => JSON.stringify(x))}`),
+    new HumanChatMessage(
+      answer
+    ),
+  ]);
+  console.log(`\nAI: ${response.text}`)
+}
+
+
+async function loadVectorStore(repositoryPath: string) {
+
+  
+  try {
+    fs.accessSync('./vector-store', fs.constants.F_OK)
+
+    console.log('Vector store exists, loading...')
+    return HNSWLib.load('./vector-store', new OpenAIEmbeddings({
+      openAIApiKey: process.env.OPEN_AI_KEY,
+    }))
+  } catch(err) {
+    const resolvedPath = path.resolve(repositoryPath)
+    console.log('Vector store does not exist, creating...')
+    console.log('Loading data from', resolvedPath, '...')
+    const configPath = ts.findConfigFile(
+      resolvedPath,
+      ts.sys.fileExists,
+      'tsconfig.json'
+    );
+
+    console.log(`tsconfig located at ${configPath}`)
+
+    if (!configPath) {
+      throw new Error('Could not find a valid tsconfig.json.');
+    }
+
+    const configFile = ts.readConfigFile(configPath, ts.sys.readFile);
+    if (configFile.error) {
+      throw new Error(dedent`
+        Errors while parsing tsconfig.
+        ${configFile.error.messageText}
+      `);
+    }
+    const parsedConfig = ts.parseJsonConfigFileContent(
+      configFile.config,
+      ts.sys,
+      path.dirname(configPath),
+      {},
+      configPath
+    );
+
+    console.log('Parsing codebase...')
+    const program = ts.createProgram({
+      rootNames: parsedConfig.fileNames,
+      options: parsedConfig.options
+    })
+    const codebaseMapping = program.getSourceFiles()
+      .filter(file => !file.fileName.includes('node_modules'))
+      .map(sourceFile => {
+        return (new TextLoader(sourceFile.fileName)).loadAndSplit()
+      })
+    const documents = (await Promise.all(codebaseMapping)).flatMap(x => x)
+    if (documents.length === 0) {
+      throw new Error(dedent`
+      Codebase couldn't be processed.
+      
+      `)
+    }
+    console.log('Creating vector store...')
+    const vectorStore = await HNSWLib.fromDocuments(documents, new OpenAIEmbeddings({
+      openAIApiKey: process.env.OPEN_AI_KEY,
+    }));
+    await vectorStore.save('./vector-store')
+    return vectorStore
+  }
+}
